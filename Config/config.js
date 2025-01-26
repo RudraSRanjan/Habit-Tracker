4c8d3c95085b2279a9236794e036160b323cc03a
RudraSRanjan <rudraranjan212@gmail.com>
2025-01-26 15:50:36 +0530

    Added some options

--- Config/config.js
+++ Config/config.js
@@ -8,7 +8,7 @@ const url= "mongodb://localhost:27017/Tracker";
 export const connectToMongoDB= async()=>{
 
     try {
-        await mongoose.connect(url);
+        await mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true });
         
         console.log("Connected to database using Mongoose");
     } catch (error) {
