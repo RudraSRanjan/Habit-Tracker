3048ce9965f89a995a5763bc28b938623b3fa9a4
RudraSRanjan <rudraranjan212@gmail.com>
2025-01-31 21:13:44 +0530

    changed mongo atlas url

--- Config/config.js
+++ Config/config.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 // dotenv.config();
 
-const url= "mongodb+srv://User:Passw0rd@tracker.zb9v5.mongodb.net/?retryWrites=true&w=majority&appName=Tracker";
+const url= "mongodb://Users:passw0rd@<hostname>/?ssl=true&replicaSet=atlas-3f7w3p-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Habits";
 
 export const connectToMongoDB= async()=>{
 
